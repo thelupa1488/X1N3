bd7d65de3ae698afa1711e7e5659cb1225dd1935
THELUPA <warking232.morozkin@hotmail.com>
2020-06-24 08:35:21 +0400

    Update KeyValues.cpp

--- Cheat/Engine/SDK/KeyValues.cpp
+++ Cheat/Engine/SDK/KeyValues.cpp
@@ -28,14 +28,14 @@ namespace SDK
     KeyValues* KeyValues::FindKey(const char* keyName, bool create)
     {
        //static auto pFindKey = reinterpret_cast<KeyValues*(__thiscall*)(KeyValues*, const char*, bool)>(Utils::PatternScan(clientFactory, XorStr("E8 ? ? ? ? F7 45")) + 1);//need fix
-        auto pFindKey = relativeToAbsolute<decltype(keyValuesFindKey)>(reinterpret_cast<int*>(Utils::PatternScan(clientFactory, "E8 ? ? ? ? F7 45") + 1));
+        auto pFindKey = relativeToAbsolute<decltype(keyValuesFindKey)>(reinterpret_cast<int*>(Utils::PatternScan(clientFactory, XorStr("E8 ? ? ? ? F7 45")) + 1));
         return pFindKey(this, keyName, create);
     }
 
     void KeyValues::SetString(const char* keyName, const char* value)
     {
         //static auto pSetSring = reinterpret_cast<void(__thiscall*)(KeyValues*, const char*)>(Utils::PatternScan(clientFactory, XorStr("E8 ? ? ? ? 89 77 38")) + 1);//need fix
-        auto pSetSring = relativeToAbsolute<decltype(keyValuesSetString)>(reinterpret_cast<int*>(Utils::PatternScan(clientFactory, "E8 ? ? ? ? 89 77 38") + 1));
+        auto pSetSring = relativeToAbsolute<decltype(keyValuesSetString)>(reinterpret_cast<int*>(Utils::PatternScan(clientFactory, XorStr("E8 ? ? ? ? 89 77 38")) + 1));
         if (const auto key = FindKey(keyName, true))
             pSetSring(key, value);
     }
